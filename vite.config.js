5f5debbd123f0a0ffca87f99df07dcd9f614eb9b
duoduoxu <duoduoxu@tencent.com>
2023-11-29 22:05:41 +0800

    feat: 改造js sample

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     rollupOptions: {
+      external: ['uxp', 'photoshop', /photoshop\/[a-z]+/],
       output: {
         format: 'cjs',
       }
